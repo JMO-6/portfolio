2a482263e67ae74c27187efc78fa03d00cca82eb
James Marshall-Osborne <80415933+JMO-6@users.noreply.github.com>
2021-05-09 16:14:02 +0100

    deploy to live (#5)
    * Added new react project and placeholder page
    
    * Added dev deploy workflow
    
    * Added dev deploy workflow
    
    * Updated development.yml
    
    * Updated development.yml
    
    * Added production deploy workflow

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to James' portfolio. Content to follow (I bet you can't
+          wait!)...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
